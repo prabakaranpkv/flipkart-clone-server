f2e1e3624f9680796d2f6cc690da09526859b412
Prabakaran <prabakaranpkv@gmail.com>
2021-12-18 00:18:29 +0530

    updaed

--- index.js
+++ index.js
@@ -35,8 +35,7 @@ paytmParams["CHANNEL_ID"] = process.env.PAYTM_CHANNEL_ID;
 paytmParams["INDUSTRY_TYPE_ID"] = process.env.PAYTM_INDUSTRY_TYPE_ID;
 paytmParams["ORDER_ID"] = uuid();
 paytmParams["CUST_ID"] = process.env.PAYTM_CUST_ID;
-paytmParams["TXN_AMOUNT"] = "100";
-paytmParams["CALLBACK_URL"] =
-  "https://flipkartclone-server.herokuapp.com/callback";
+paytmParams["TXN_AMOUNT"] = "1200";
+paytmParams["CALLBACK_URL"] = "http://localhost:8000/callback";
 paytmParams["EMAIL"] = "prabakaranpkv@gmail.com";
 paytmParams["MOBILE_NO"] = "1234567898";
